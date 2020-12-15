9bbdaaee26179cc15c329099c44c872da1b7cc8e
MichaelRuhwedel <michael.ruhwedel@gmail.com>
2020-12-15 10:42:23 +0100

    refactor(*) remove dead code

--- src/main/java/com/mruhwedel/banking/account/TransferResult.java
+++
@@ -1,9 +0,0 @@
-package com.mruhwedel.banking.account;
-
-public enum TransferResult {
-    TRANSFERRED,
-    INSUFFICIENT_FUNDS,
-    CANNOT_WITHDRAW_FROM_LOAN,
-    INVALID_ACCOUNT_TARGET,
-    ACCOUNT_NONEXISTENT
-}
